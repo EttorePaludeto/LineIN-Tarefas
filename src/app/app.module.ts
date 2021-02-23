509e19fb8daf3f0e5f2424a621cf50074e87177b
EttorePaludeto <ettorepaludeto@gmail.com>
2021-02-23 08:38:18 -0300

    UI inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TarefaCadastroComponent } from './tarefa-cadastro/tarefa-cadastro.component';
 import { TarefaListaComponent } from './tarefa-lista/tarefa-lista.component';
+import { FormDebugComponent } from './form-debug/form-debug.component';
 
 @NgModule({
   declarations: [	
     AppComponent,
       TarefaCadastroComponent,
-      TarefaListaComponent
+      TarefaListaComponent,
+      FormDebugComponent
    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/tarefa-cadastro/tarefa-cadastro.component.ts
+++ src/app/tarefa-cadastro/tarefa-cadastro.component.ts
@@ -1,14 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import {DatePipe} from '@angular/common';
 
 @Component({
   selector: 'app-tarefa-cadastro',
   templateUrl: './tarefa-cadastro.component.html',
+  providers: [DatePipe]
 })
 export class TarefaCadastroComponent implements OnInit {
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, private datePipe: DatePipe) { }
+
+  tarefa : FormGroup;
+
+  
 
   ngOnInit() {
+      this.tarefa = this.formBuilder.group({
+        DtCadastro: [this.datePipe.transform(Date.now(), 'dd/MM/yyyy')],
+        DtEntrega: [this.datePipe.transform(Date.now(), 'dd/MM/yyyy')],
+        Empresa: [],
+        Titulo: [],
+        Pessoas: [],
+        Descricao: [],
+        Tag: [],
+        Situacao: []
+    })
+  }
+
+  onSubmit(){
+    console.log(this.tarefa)
   }
 
 }
